53ddd05b30258d47cfd255d7c7436d4cf96d58a9
dmitry <dmtrbrlkv@gmail.com>
2020-04-13 12:43:25 +0300

    install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ip2w',
+    version='',
+    packages=['uwsgi', 'requests'],
+    url='',
+    license='',
+    author='dmitry',
+    author_email='',
+    description=''
+)
